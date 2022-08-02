92832960235aca8fec25b0c215dbafe4e36eb819
Samrat Kundu <47251618+Samrat2000@users.noreply.github.com>
2022-08-02 16:34:42 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] s) {
-        System.out.println("Hello All");
-        for(int i = 0; i < 20; i++) {
+        System.out.println("Hello Oracle");
+        for(int i = 0; i < 10; i++) {
             System.out.println("Value of i: " + i);
         }
     }
